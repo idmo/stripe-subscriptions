1357c4c52a45ca9ed1859249491f806165b36670
Brian Maggi <brianmaggi@gmail.com>
2021-01-19 15:11:44 -0800

    fix handle sub

--- functions/handle-subscription-change.js
+++ functions/handle-subscription-change.js
@@ -1,56 +1,63 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
-const fetch = require*('node-fetch')
-const {faunaFetch} = require('./utils/fauna')
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const fetch = require('node-fetch');
+const { faunaFetch } = require('./utils/fauna');
 
-exports.handler = async ({ body, headers} , context) => {
-    try {
-        const stripeEvent = stripe.webhooks.constructEvent(
-            body,
-            headers['stripe-signature'],
-            process.env.STRIPE_WEBHOOK_SECRET,
-        );
+exports.handler = async ({ body, headers }, context) => {
+  try {
+    // make sure this event was sent legitimately.
+    const stripeEvent = stripe.webhooks.constructEvent(
+      body,
+      headers['stripe-signature'],
+      process.env.STRIPE_WEBHOOK_SECRET,
+    );
 
-        if (stripeEvent.type !== 'customer.subscription.updated') return;
+    // bail if this is not a subscription update event
+    if (stripeEvent.type !== 'customer.subscription.updated') return;
 
-        const subscription = stripeEvent.data.object
+    const subscription = stripeEvent.data.object;
 
-        const result  = await faunaFetch({
-            query: `
-            query($stripeID: ID!) {
-                getUserByStripeID(stripeID: $stripeID) {
-                    netlifyID
-                }
+    const result = await faunaFetch({
+      query: `
+          query ($stripeID: ID!) {
+            getUserByStripeID(stripeID: $stripeID) {
+              netlifyID
             }
-            `,
-            variables: {
-                stripeID: subscription.customer,
-            },
-        });
-        const { netlifyID } = result.data.getUserByStripeID;
+          }
+        `,
+      variables: {
+        stripeID: subscription.customer,
+      },
+    });
 
-        const plan = subscription.items.data[0].plan.nickname;
-        const role = plan.split(' ')[0].toLowerCase();
+    const { netlifyID } = result.data.getUserByStripeID;
 
-        const {identity} = context.clientContext;
-        await fetch(`${identity.url}/admin/users/${netlifyID}`, {
-            method: 'PUT',
-            headers: {
-                Authorization: `Bearer ${identity.token}`,
-            },
-            body: JSON.stringify({
-                app_metadata: {
-                    roles: [role],
-                },
-            }),
-        });
-        return {
-            statusCode: 200,
-            body: JSON.stringify({ received: true}),
-        };
-    } catch (err) {
-        return {
-            statusCode: 400,
-            body: `Webhook Error: ${error.message}`
-        }
-    }
-}
+    // take the first word of the plan name and use it as the role
+    const plan = subscription.items.data[0].plan.nickname;
+    const role = plan.split(' ')[0].toLowerCase();
+
+    // send a call to the Netlify Identity admin API to update the user role
+    const { identity } = context.clientContext;
+    await fetch(`${identity.url}/admin/users/${netlifyID}`, {
+      method: 'PUT',
+      headers: {
+        // note that this is a special admin token for the Identity API
+        Authorization: `Bearer ${identity.token}`,
+      },
+      body: JSON.stringify({
+        app_metadata: {
+          roles: [role],
+        },
+      }),
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ received: true }),
+    };
+  } catch (err) {
+    return {
+      statusCode: 400,
+      body: `Webhook Error: ${err.message}`,
+    };
+  }
+};
