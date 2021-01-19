ef33dd13720283638e9f9388081e3a4b890a113b
Brian Maggi <brianmaggi@gmail.com>
2021-01-19 14:29:21 -0800

    fixed function

--- functions/create-manage-link.js
+++ functions/create-manage-link.js
@@ -1,29 +1,31 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRETE_KEY)
-const {faunaFetch} = require('./utils/fauna')
-
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const { faunaFetch } = require('./utils/fauna');
 
 exports.handler = async (_event, context) => {
-    const { user } = context.clientContext
-    const result = await faunaFetch({
-        query: `
-        query ($netlifyID: ID!){
-            getUserByNetlifyID(netlifyID: $netlifyID) {
-                stripeID
-            }
-        }
-        `,
-        variables: {
-            netlifyID: user.sub,
-        },
-    });
-    const {stripeID} = result.data.getUserByNetlifyID;
-    const link = await stripe.billingPortal.sessions.create({
-        customer : stripeID,
-        return_url: process.env.URL,
-    });
+  const { user } = context.clientContext;
 
-    return {
-        statusCode: 200,
-        body: JSON.stringify(link.url)
-    }
-}
+  const result = await faunaFetch({
+    query: `
+      query ($netlifyID: ID!) {
+        getUserByNetlifyID(netlifyID: $netlifyID) {
+          stripeID
+        }
+      }
+    `,
+    variables: {
+      netlifyID: user.sub,
+    },
+  });
+
+  const { stripeID } = result.data.getUserByNetlifyID;
+
+  const link = await stripe.billingPortal.sessions.create({
+    customer: stripeID,
+    return_url: process.env.URL,
+  });
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(link.url),
+  };
+};
